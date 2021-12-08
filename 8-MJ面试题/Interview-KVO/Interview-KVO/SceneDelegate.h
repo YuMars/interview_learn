98d626c6eb54d4159e150a14c7821eeb986ede0a
Red-Fish <365795976@qq.com>
2021-12-08 13:51:47 +0800

    kvo

---
+++ 8-MJ面试题/Interview-KVO/Interview-KVO/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Interview-KVO
+//
+//  Created by Red-Fish on 2021/12/8.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ 8-MJ面试题/Interview-KVO/Interview-KVO/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  Interview-KVO
+//
+//  Created by Red-Fish on 2021/12/8.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Q:iOS用什么方式实现一个对象的KVO？（KVO的本质是什么）
+    
+    // Q:如何手动触发KVO？
+    
+}
+
+
+@end
