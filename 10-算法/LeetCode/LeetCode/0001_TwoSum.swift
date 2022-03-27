da64ef352ab55271824835916f31abdc4238f013
Red-Fish <365795976@qq.com>
2022-03-27 23:41:26 +0800

    leet code 算法

---
+++ 10-算法/LeetCode/LeetCode/0001_TwoSum.swift
@@ -0,0 +1,23 @@
+//
+//  0001_TwoSum.swift
+//  LeetCode
+//
+//  Created by Red-Fish on 2022/3/27.
+//
+
+import Foundation
+
+
+class TwoSum {
+    func twoSum(_ nums:[Int], _ target:Int) -> [Int] {
+        for i in 0...nums.count - 1 {
+            for j in i + 1..<nums.count {
+                if nums[i] + nums[j] == target {
+                    return [i, j]
+                }
+            }
+        }
+        return []
+        
+    }
+}
---
+++ 10-算法/LeetCode/LeetCode/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  LeetCode
+//
+//  Created by Red-Fish on 2022/3/27.
+//
+
+import Foundation
+
+print("Hello, World!")
+
