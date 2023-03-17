f618d5e17bad8f44a09f5a50eb4cde6346a85259
Red-Fish <365795976@qq.com>
2023-03-17 17:46:32 +0800

    事件手势响应链

--- 18-Interview-TouchEvent/TouchEvent/TouchEvent/HitTestView.m
+++ 18-Interview-TouchEvent/TouchEvent/TouchEvent/HitTestView.m
@@ -37,4 +37,11 @@
     return self;
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    // 默认会把事件传递给上一个响应者,上一个响应者是父控件,交给父控件处理
+    [super touchesBegan:touches withEvent:event];
+    // 注意不是调用父控件的touches方法，而是调用父类的touches方法
+    // super是父类 superview是父控件
+}
+
 @end
