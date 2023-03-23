3af059bd2d9bf7fc7937d12eb3550681d80d1d04
Red-Fish <365795976@qq.com>
2023-03-23 20:49:20 +0800

    Mach-O

---
+++ 00-Interview-源码/machoview-obaby-branch-main/MachOView-M1/build/machoview.build/Release/MachOView.build/Objects-normal/x86_64/AppController.d
@@ -0,0 +1,19 @@
+dependencies: /Users/zhongming/xcode_projects/MachOView/mach-o/swap.h \
+  /Users/zhongming/xcode_projects/MachOView/mach-o/compact_unwind_encoding.h \
+  /Users/zhongming/xcode_projects/MachOView/mach-o/ranlib.h \
+  /Users/zhongming/xcode_projects/MachOView/mach-o/arm64/reloc.h \
+  /Users/zhongming/xcode_projects/MachOView/mach-o/arm/reloc.h \
+  /Users/zhongming/xcode_projects/MachOView/mach-o/x86_64/reloc.h \
+  /Users/zhongming/xcode_projects/MachOView/mach-o/reloc.h \
+  /Users/zhongming/xcode_projects/MachOView/mach-o/nlist.h \
+  /Users/zhongming/xcode_projects/MachOView/mach-o/fat.h \
+  /Users/zhongming/xcode_projects/MachOView/mach-o/loader.h \
+  /Users/zhongming/xcode_projects/MachOView/mach-o/arch.h \
+  /Users/zhongming/xcode_projects/MachOView/Prefix.pch \
+  /Users/zhongming/xcode_projects/MachOView/AppController.mm \
+  /Users/zhongming/xcode_projects/MachOView/Common.h \
+  /Users/zhongming/xcode_projects/MachOView/AppController.h \
+  /Users/zhongming/xcode_projects/MachOView/DataController.h \
+  /Users/zhongming/xcode_projects/MachOView/Document.h \
+  /Users/zhongming/xcode_projects/MachOView/PreferenceController.h \
+  /Users/zhongming/xcode_projects/MachOView/Attach.h
---
+++ 00-Interview-源码/machoview-obaby-branch-main/MachOView-M1/build/machoview.build/Release/MachOView.build/Objects-normal/x86_64/MachOLayout.d
@@ -0,0 +1,25 @@
+dependencies: /Users/zhongming/xcode_projects/MachOView/mach-o/swap.h \
+  /Users/zhongming/xcode_projects/MachOView/mach-o/compact_unwind_encoding.h \
+  /Users/zhongming/xcode_projects/MachOView/mach-o/ranlib.h \
+  /Users/zhongming/xcode_projects/MachOView/mach-o/arm64/reloc.h \
+  /Users/zhongming/xcode_projects/MachOView/mach-o/arm/reloc.h \
+  /Users/zhongming/xcode_projects/MachOView/mach-o/x86_64/reloc.h \
+  /Users/zhongming/xcode_projects/MachOView/mach-o/reloc.h \
+  /Users/zhongming/xcode_projects/MachOView/mach-o/nlist.h \
+  /Users/zhongming/xcode_projects/MachOView/mach-o/fat.h \
+  /Users/zhongming/xcode_projects/MachOView/mach-o/loader.h \
+  /Users/zhongming/xcode_projects/MachOView/mach-o/arch.h \
+  /Users/zhongming/xcode_projects/MachOView/Prefix.pch \
+  /Users/zhongming/xcode_projects/MachOView/MachOLayout.mm \
+  /Users/zhongming/xcode_projects/MachOView/Common.h \
+  /Users/zhongming/xcode_projects/MachOView/MachOLayout.h \
+  /Users/zhongming/xcode_projects/MachOView/Layout.h \
+  /Users/zhongming/xcode_projects/MachOView/DataController.h \
+  /Users/zhongming/xcode_projects/MachOView/ReadWrite.h \
+  /Users/zhongming/xcode_projects/MachOView/LoadCommands.h \
+  /Users/zhongming/xcode_projects/MachOView/LinkEdit.h \
+  /Users/zhongming/xcode_projects/MachOView/DyldInfo.h \
+  /Users/zhongming/xcode_projects/MachOView/Exceptions.h \
+  /Users/zhongming/xcode_projects/MachOView/SectionContents.h \
+  /Users/zhongming/xcode_projects/MachOView/ObjC.h \
+  /Users/zhongming/xcode_projects/MachOView/CRTFootPrints.h
