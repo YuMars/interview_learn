93bc6fb9b31cd084d14d5227077ad785c888560e
Red-Fish <365795976@qq.com>
2024-07-30 09:06:54 +0800

    leetcode

--- 00-Interview-算法/LeetCode/LeetCode/0075_SortColors.swift
+++ 00-Interview-算法/LeetCode/LeetCode/0075_SortColors.swift
@@ -37,7 +37,30 @@ public class SortColors {
     
     /// 快速排序
     public class func sortColors1(_ nums: inout [Int]) {
+        var i: Int = 0
         var left: Int = 0
         var right: Int = nums.count - 1
+        
+        // 区间 i是循环变量，所以在当i <= right的时候循环结束,[left, i)是右开区间，是因为如果i是右闭区间，那么循环开始的时候在[left, i)中就会存在一个元素
+        // [0, left) == 0
+        // [left, i) == 1
+        // (right, len - 1) == 2
+        while i <= right {
+            if nums[i] == 0 {
+                swap(&nums, i, left)
+                i += 1
+            } else if nums[i] == 1 {
+                i += 1
+            } else { // num[i] == 2
+                swap(&nums, i, right)
+                right -= 1
+            }
+        }
+    }
+    
+    public class func swap(_ nums: inout  [Int], _ left: Int, _ right: Int) {
+        let temp: Int = nums[left]
+        nums[left] = nums[right]
+        nums[right] = temp
     }
 }
